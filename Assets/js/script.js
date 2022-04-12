ba80e983a83d40f1c4b174d249978842de16b966
mcgidoug <mcgidoug@gmail.com>
2022-04-12 14:21:41 -0500

    updated js functionality and page layout

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,10 @@
-let container = document.querySelector("#container");
-
+let currentImg = document.querySelector("#currentImg");
+let currentQuote = document.querySelector("#currentQuote");
 // quote generator fetch
 
 fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes")
   .then((response) => response.json())
-  .then((data) => (container.innerHTML = data));
+  .then((data) => (currentQuote.innerHTML = data));
 // let container = document.querySelector("#container")
 
 fetch(`https://api.thecatapi.com/v1/images/search`, {})
@@ -14,5 +14,6 @@ fetch(`https://api.thecatapi.com/v1/images/search`, {})
   })
   .then(function (data) {
     console.log(data);
+    currentImg.setAttribute("src", data[0].url);
     // container.innerHTML = data.
   });
